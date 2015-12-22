8a362bbd1affc0e461d6f0d8d7a5f0b5f2950890
Xmart <xmart@xmartdemac-mini.local>
2015-12-22 15:58:29 +0800

    Initial Commit

---
+++ line_animation/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  line_animation
+//
+//  Created by Xmart on 15/12/22.
+//  Copyright © 2015年 Xmart. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
